f0e446ab3406c98f2f83bb9db977d64d84547cbc
alifrontend499 <alifrontend96@gmail.com>
2019-05-19 19:42:24 +0530

    home page and login completer

--- projects/gangwar_institute/js/app.js
+++ projects/gangwar_institute/js/app.js
@@ -4,8 +4,98 @@
     (function () {
         document.addEventListener('DOMContentLoaded', function () {
             var elems = document.querySelectorAll('.sidenav');
-            console.log(elems);
             var instances = M.Sidenav.init(elems, {});
         });
     })();
+})();
+
+// SLIDER
+(function () {
+    $(function () {
+        // ALL CONTENT SLIDER
+        $('#all-content-slider').owlCarousel({
+            items: 5,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplaySpeed: 1000,
+            responsive: {
+                0: {
+                    items: 5
+                },
+                1000: {
+                    items: 8
+                }
+            }
+        });
+        // ALL CONTENT SLIDER
+        $('#online-test-slider').owlCarousel({
+            items: 5,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplaySpeed: 1000,
+            responsive: {
+                0: {
+                    items: 5
+                },
+                1000: {
+                    items: 8
+                }
+            }
+        });
+        // ALL CONTENT SLIDER
+        $('#video-solutions-slider').owlCarousel({
+            items: 3,
+            margin: 10,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplaySpeed: 1000,
+            merge: true
+        });
+        // STUDY-MATERIAL-SLIDER
+        $('#study-material-slider').owlCarousel({
+            items: 3,
+            margin: 10,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplaySpeed: 1000,
+            merge: true
+        });
+    });
+})();
+
+// HEADER FIX
+(function () {
+    $(window).on("load scroll", function () {
+        // HOMEPAGE
+        (function () {
+            var app_headHeight = $('header').innerHeight();
+            // var app_mainmenuHeight = $('#main-menu').innerHeight();
+            // if ($(this).scrollTop() > app_headHeight + app_mainmenuHeight) {
+            if ($(this).scrollTop() > app_headHeight) {
+                $('.app-head-top').addClass('fixed');
+            } else {
+                $('.app-head-top').removeClass('fixed');
+            }
+        })();
+    });
+})();
+
+(function () {
+    $(function () {
+        // BOTTOM BAR MENU
+        if ($('body').find('.bottom-menu-bar').length) {
+            $('body').addClass('hasBMB');
+        } else {
+            $('body').removeClass('hasBMB');
+        };
+        // FANCYBOX
+        $('[data-fancybox]').fancybox({
+            animationEffect: "zoom-in-out"
+        });
+    });
 })();
