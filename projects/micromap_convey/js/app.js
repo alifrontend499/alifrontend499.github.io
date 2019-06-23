a68490fecb8d426c75b7924b5cea2cd82615e9e5
alifrontend499 <alifrontend96@gmail.com>
2019-06-23 18:13:22 +0530

    changes live upfated

---
+++ projects/micromap_convey/js/app.js
@@ -0,0 +1,9 @@
+$(function () {
+    // HOME SLIDER
+    $('#home-slider').owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000
+    })
+});
