18ae6e1150f20258869e55dd5d54db4d4b6be46f
alifrontend499 <alifrontend96@gmail.com>
2019-10-01 20:30:34 +0530

    new project added

--- projects/bioneet_mcq/js/app.js
+++ projects/bioneet_mcq/js/app.js
@@ -9,21 +9,6 @@
     })();
 })();
 $(function () {
-    // APP HEAD SEARCH
-    (function () {
-        $('.app-head-search > a').click(function (ev) {
-            ev.preventDefault();
-            $(this).next().addClass('active').find('input').focus();
-        });
-        $('.app-head-search > a,.app-head-search .search').click(function (ev) {
-            ev.stopPropagation();
-        });
-        // CLOSING SEARCH
-        $(document).click(function () {
-            $('.app-head-search .search').removeClass('active');
-        });
-    })();
-
     // APP APP MORE MENU
     (function () {
         $('.app-more-menu > a').click(function (ev) {
@@ -42,74 +27,16 @@ $(function () {
 
 // HEADER FIX
 (function () {
-    $(window).on("load scroll", function () {
-        // HOMEPAGE
-        (function () {
-            var app_headHeight = $('header').innerHeight();
-            var app_bbHeight = $('.backBar').innerHeight();
-            // HEADER FIXED
-            if ($(this).scrollTop() > app_headHeight) {
-                $('.app-head-top').addClass('fixed');
-            } else {
-                $('.app-head-top').removeClass('fixed');
-            }
-
-            // BACKBTN FIXED
-            if ($(this).scrollTop() > app_bbHeight) {
-                $('.backBar >.BB_top').addClass('fixed');
-            } else {
-                $('.backBar>.BB_top').removeClass('fixed');
-            }
-        })();
-    });
-})();
-
-(function () {
-    $(function () {
-        // BOTTOM BAR MENU
-        if ($('body').find('.bottom-menu-bar').length) {
-            $('body').addClass('hasBMB');
-        } else {
-            $('body').removeClass('hasBMB');
-        };
-        // FANCYBOX
-        $('[data-fancybox]').fancybox({
-            animationEffect: "zoom-in-out"
-        });
-    });
-})();
-// GO BACK
-function goBack() {
-    window.history.back();
-}
-
-
-(function () {
-    $('.A-ques .open-answer').click(function (ev) {
-        ev.preventDefault();
-        ($('.material-icons', this).text() === 'keyboard_arrow_up') ?
-            $('.material-icons', this).text('keyboard_arrow_down') : $('.material-icons', this).text('keyboard_arrow_up');
-        $(this).closest('.ques').next().slideToggle(100, function () {
-            $(this).parent().toggleClass('in')
-        });
-    });
-})();
-
-(function () {
-    $('.sel-ans label > input').change(function () {
+    $('.sel-ans label.checkbox > input').change(function () {
         if ($(this).is(':checked')) {
             $(this).parent().addClass('active');
         } else {
             $(this).parent().removeClass('active')
         }
     });
-})();
-
-(function () {
-    $('.app-open-menu a').click(function () {
-        var elem = $(this).data('open');
-        $(this).toggleClass('active').find('i').text(($('i', this).text() == 'keyboard_arrow_right') ? 'keyboard_arrow_left' : 'keyboard_arrow_right');
-
-        $('#test-info-sidebar').toggleClass('active');
+    $('.sel-ans label.radio > input').change(function () {
+        if ($(this).is(':checked')) {
+            $(this).parent().addClass('active').siblings().removeClass('active');
+        }
     });
 })();
---
+++ projects/bukhar/js/app.js
@@ -0,0 +1,106 @@
+$(function () {
+    // HOME SLIDER
+    $('#home-slider').owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000
+    });
+    // PARTNERS SLIDER
+    $('#partners-slider').owlCarousel({
+        items: 7,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            400: {
+                items: 3
+            },
+            768: {
+                items: 4,
+            },
+            1200: {
+                items: 6,
+            },
+            1400: {
+                items: 7,
+            }
+        }
+    });
+    // COMPANIES SLIDER
+    $('#companies-slider').owlCarousel({
+        items: 6,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            400: {
+                items: 3
+            },
+            768: {
+                items: 4,
+            },
+            1200: {
+                items: 6,
+            }
+        }
+    });
+    // SIMILAR PRODUCTS SLIDER
+    $('#similar-prods-slider').owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000
+    });
+
+    (function () {
+        $('.open-BP-modal').click(function (ev) {
+            ev.preventDefault();
+            var modal = $(this).data('modal');
+            $('.modal').modal('hide');
+            setTimeout(function() {
+                $(modal).modal('show');
+            }, 150)
+        });
+    })();
+
+    // OPEN MOBILE MENU
+    (function() {
+        $('.mob-menu-btn > a').click(function(ev) {
+            ev.preventDefault();
+            $(this).toggleClass('active').find('.fas').toggleClass('fa-times fa-bars');
+            $('.head_main .menu').toggleClass('active');
+            $('.menu .mob-menu-overlay').fadeToggle(100);
+        });
+        $('.menu .mob-menu-overlay').click(function(ev) {
+            ev.preventDefault();
+            $('.mob-menu-btn > a').removeClass('active').find('.fas').removeClass('fa-times').addClass('fa-bars');
+            $('.head_main .menu').removeClass('active');
+            $('.menu .mob-menu-overlay').fadeOut(100);
+        });
+    })();
+    
+    // OPEN MOBILE FILTER PRODUCTS
+    (function() {
+        if($(window).width() < 768) {
+            $('.mob-filter-btn > a').click(function(ev) {
+                ev.preventDefault();
+                ev.stopPropagation();
+                $(this).toggleClass('active');
+                $('.ST_products .right-content').toggleClass('active');
+                $('.ST_products .leftbar').fadeToggle(300);
+            });
+            $('.ST_products .right-content').click(function() {
+                $('.mob-filter-btn > a').removeClass('active');
+                $(this).removeClass('active');
+                $('.ST_products .leftbar').fadeOut(300);
+            });
+        }
+    })();
+});
