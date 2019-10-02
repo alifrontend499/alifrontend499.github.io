bf54fb73ab47bc8eea843125ebe9ebb47063b56d
alifrontend499 <alifrontend96@gmail.com>
2019-10-02 21:54:22 +0530

    new changes homepage bukhar

--- projects/bukhar/js/app.js
+++ projects/bukhar/js/app.js
@@ -6,12 +6,15 @@ $(function () {
         autoplay: true,
         autoplayTimeout: 2000
     });
+
     // PARTNERS SLIDER
-    $('#partners-slider').owlCarousel({
-        items: 7,
+    $('#speciality-slider').owlCarousel({
+        items: 4,
         dots: false,
         autoplay: true,
-        autoplayTimeout: 2000,
+        autoplayTimeout: 200000,
+        nav: true,
+        navText: ['<i class="material-icons">keyboard_arrow_left</i>', ' <i class="material-icons">keyboard_arrow_right</i>'],
         responsive: {
             0: {
                 items: 2
@@ -21,42 +24,8 @@ $(function () {
             },
             768: {
                 items: 4,
-            },
-            1200: {
-                items: 6,
-            },
-            1400: {
-                items: 7,
             }
         }
-    });
-    // COMPANIES SLIDER
-    $('#companies-slider').owlCarousel({
-        items: 6,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        responsive: {
-            0: {
-                items: 2
-            },
-            400: {
-                items: 3
-            },
-            768: {
-                items: 4,
-            },
-            1200: {
-                items: 6,
-            }
-        }
-    });
-    // SIMILAR PRODUCTS SLIDER
-    $('#similar-prods-slider').owlCarousel({
-        items: 1,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 2000
     });
 
     (function () {
@@ -64,43 +33,25 @@ $(function () {
             ev.preventDefault();
             var modal = $(this).data('modal');
             $('.modal').modal('hide');
-            setTimeout(function() {
+            setTimeout(function () {
                 $(modal).modal('show');
             }, 150)
         });
     })();
 
     // OPEN MOBILE MENU
-    (function() {
-        $('.mob-menu-btn > a').click(function(ev) {
+    (function () {
+        $('.mob-menu-btn > a').click(function (ev) {
             ev.preventDefault();
             $(this).toggleClass('active').find('.fas').toggleClass('fa-times fa-bars');
             $('.head_main .menu').toggleClass('active');
             $('.menu .mob-menu-overlay').fadeToggle(100);
         });
-        $('.menu .mob-menu-overlay').click(function(ev) {
+        $('.menu .mob-menu-overlay').click(function (ev) {
             ev.preventDefault();
             $('.mob-menu-btn > a').removeClass('active').find('.fas').removeClass('fa-times').addClass('fa-bars');
             $('.head_main .menu').removeClass('active');
             $('.menu .mob-menu-overlay').fadeOut(100);
         });
-    })();
-    
-    // OPEN MOBILE FILTER PRODUCTS
-    (function() {
-        if($(window).width() < 768) {
-            $('.mob-filter-btn > a').click(function(ev) {
-                ev.preventDefault();
-                ev.stopPropagation();
-                $(this).toggleClass('active');
-                $('.ST_products .right-content').toggleClass('active');
-                $('.ST_products .leftbar').fadeToggle(300);
-            });
-            $('.ST_products .right-content').click(function() {
-                $('.mob-filter-btn > a').removeClass('active');
-                $(this).removeClass('active');
-                $('.ST_products .leftbar').fadeOut(300);
-            });
-        }
     })();
 });
