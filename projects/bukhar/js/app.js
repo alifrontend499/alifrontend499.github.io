54d8d9348a4d1c7970c0a1bdcdfb0a6c94076ccf
alifrontend499 <alifrontend96@gmail.com>
2019-10-06 15:20:47 +0530

    new changes done

--- projects/bukhar/js/app.js
+++ projects/bukhar/js/app.js
@@ -28,30 +28,5 @@ $(function () {
         }
     });
 
-    (function () {
-        $('.open-BP-modal').click(function (ev) {
-            ev.preventDefault();
-            var modal = $(this).data('modal');
-            $('.modal').modal('hide');
-            setTimeout(function () {
-                $(modal).modal('show');
-            }, 150)
-        });
-    })();
-
-    // OPEN MOBILE MENU
-    (function () {
-        $('.mob-menu-btn > a').click(function (ev) {
-            ev.preventDefault();
-            $(this).toggleClass('active').find('.fas').toggleClass('fa-times fa-bars');
-            $('.head_main .menu').toggleClass('active');
-            $('.menu .mob-menu-overlay').fadeToggle(100);
-        });
-        $('.menu .mob-menu-overlay').click(function (ev) {
-            ev.preventDefault();
-            $('.mob-menu-btn > a').removeClass('active').find('.fas').removeClass('fa-times').addClass('fa-bars');
-            $('.head_main .menu').removeClass('active');
-            $('.menu .mob-menu-overlay').fadeOut(100);
-        });
-    })();
+    
 });
