ec2957977b5c9515bb3090699b26e84c2943c29b
Ali Mir <ali@vezire.com>
2020-12-06 23:01:43 +0530

    new changes | fashion 2

--- projects/CURRENT/fashionone2/js/app.js
+++ projects/CURRENT/fashionone2/js/app.js
@@ -23,4 +23,39 @@ $(window).on("onreadystatechange contentLoaded scroll", function () {
 
 $(function () {
 
+    // SHOW MORE LESS CONTAINER
+    (function () {
+        $('.show-more-container > .show-more-less').text('show more')
+        $('.show-more-container > .show-more-less').click(function (ev) {
+            ev.preventDefault()
+            $(this).prev().toggleClass('less-content')
+            $(this).toggleClass('active')
+            $(this).text($(this).text().trim().toLowerCase() === 'show more' ? 'show less' : 'show more')
+        })
+    })();
+
+    // ALL SLIDERS
+    (function () {
+        // RECENT VIDEO SLIDER
+        $('#recent-videos-slider').owlCarousel({
+            // loop: true,
+            margin: 8,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                600: {
+                    items: 5,
+                }
+            }
+        })
+
+        // HOMEPAGE SLIDER
+        $('#home-slider').owlCarousel({
+            items: 1,
+            margin: 8,
+            nav: true,
+        })
+    })();
 });
