dd5acae8fdf0ad110a7bb042b73883b703d96c7b
Ali Mir <ali@vezire.com>
2020-12-08 01:01:12 +0530

    new changes done in the | fashion 2

--- projects/CURRENT/fashionone2/js/app.js
+++ projects/CURRENT/fashionone2/js/app.js
@@ -41,6 +41,7 @@ $(function () {
             // loop: true,
             margin: 8,
             nav: true,
+            dots: false,
             responsive: {
                 0: {
                     items: 2
@@ -54,8 +55,60 @@ $(function () {
         // HOMEPAGE SLIDER
         $('#home-slider').owlCarousel({
             items: 1,
-            margin: 8,
+            loop: true,
+            margin: 5,
             nav: true,
+            dots: false,
+            navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            smartSpeed: 2000,
         })
+
+        // HEADLINE SLIDER
+        $('#headline-slider').owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            dots: false,
+            mouseDrag: false,
+            touchDrag: false,
+            pullDrag: false,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            smartSpeed: 1000,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut',
+
+        })
+    })();
+
+    // DYNAMIC TABS
+    (function () {
+        var navCount = 0;
+        var tabContentCount = 0;
+        $('.st-dynamic-tabs > li').each(function () {
+            // console.log(navCount);
+            $('>a', this).attr('id', 'aria-tab-' + navCount);
+            $('>a', this).attr('href', '#href-' + navCount);
+            $('>a', this).attr('aria-controls', 'href-' + navCount);
+            navCount += 1;
+        });
+        $('.st-dynamic-tabs + .tab-content > .tab-pane').each(function () {
+            // console.log(tabContentCount);
+            $(this).attr('aria-labelledby', 'aria-tab-' + tabContentCount)
+            $(this).attr('id', 'href-' + tabContentCount)
+            tabContentCount += 1;
+        });
+    })();
+
+    // WEEK TABS
+    (function () {
+        $('.week-tabs-container .week-day-name').click(function (ev) {
+            ev.preventDefault()
+            $(this).addClass('active').parent().siblings().find('a').removeClass('active')
+        });
     })();
 });
