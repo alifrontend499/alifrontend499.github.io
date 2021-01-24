82b4be6275375590888c51b05a9623f61d3a7c5a
Ali Mir <ali@vezire.com>
2021-01-24 22:48:15 +0530

    new site | dr-hozpito | made some sections | and moved cuurent site to the websites

---
+++ projects/CURRENT/dr-hozpito/js/app.js
@@ -0,0 +1,200 @@
+$(window).on('load scroll', function () {
+    const headerHeight = $('header').innerHeight()
+    console.log(headerHeight)
+
+    // $('.header-like-margin-top').css('margin-top', headerHeight)
+    // $('.header-like-padding-top').css('padding-top', headerHeight)
+})
+
+$(function () {
+    // HOME SLIDER
+    $('#home-slider').owlCarousel({
+        items: 1,
+        // loop: true,
+        dots: false,
+        nav: true,
+        navText: ['<i class="feather-chevron-left"></i>', '<i class="feather-chevron-right"></i>'],
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 3000,
+    });
+
+    // CATEGORY SLIDER
+    $('#category-slider').owlCarousel({
+        items: 6,
+        // loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 3200,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 4,
+            },
+            992: {
+                items: 6,
+            },
+        }
+    });
+
+    // RELATED PRODUCTS SLIDER
+    $('#related-products-slider').owlCarousel({
+        items: 6,
+        // loop: true,
+        nav: true,
+        navText: ['<i class="feather-chevron-left"></i>', '<i class="feather-chevron-right"></i>'],
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 2800,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 4,
+            },
+            992: {
+                items: 5,
+            },
+        }
+    });
+
+    // OUR PRODUCTS SLIDER
+    $('#our-products-slider').owlCarousel({
+        items: 6,
+        // loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 2800,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 4,
+            },
+            992: {
+                items: 6,
+            },
+        }
+    });
+
+    // HOME FOUR BLOCKS SLIDER
+    $('#home-four-blocks-slider').owlCarousel({
+        items: 4,
+        // loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 2700,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 4,
+            },
+        }
+    });
+
+    // HEALTH EXPERTS SLIDER
+    $('#health-experts-slider').owlCarousel({
+        items: 3,
+        // loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 3400,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                margin: 20,
+            },
+            1200: {
+                margin: 50,
+            },
+        }
+    });
+
+    // PRODUCT DETAILS THUMBNAIL SLIDER
+    $('#product-details-thumbnail-slider').owlCarousel({
+        items: 3,
+        dots: false,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        margin: 5,
+        responsive: {
+            0: {
+                items: 3,
+            },
+            576: {
+                items: 3,
+            }
+        }
+    });
+
+
+    // ST-DYNAMIC-TABS
+    (function () {
+        var navCount = 0;
+        var tabContentCount = 0;
+        $('.st-dynamic-tabs > li').each(function () {
+            $('>a', this).attr('id', 'aria-tab-' + navCount);
+            $('>a', this).attr('href', '#href-' + navCount);
+            $('>a', this).attr('aria-controls', 'href-' + navCount);
+            navCount += 1;
+        });
+        $('.st-dynamic-tabs + .tab-content > .tab-pane, .st-dynamic-tabs-content > .tab-pane').each(function () {
+            $(this).attr('aria-labelledby', 'aria-tab-' + tabContentCount)
+            $(this).attr('id', 'href-' + tabContentCount)
+            tabContentCount += 1;
+        });
+    })();
+
+    // OPEN TABS
+    (function () {
+        $('[data-open-tab]').click(function (ev) {
+            ev.preventDefault();
+            $('#' + $(this).attr('data-open-tab')).trigger('click');
+        });
+    })();
+});
