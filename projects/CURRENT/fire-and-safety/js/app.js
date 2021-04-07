c93cb02fee03ced21ea46b8d6057ff11d2286135
alifrontend499 <alifrontend96@gmail.com>
2021-04-07 22:25:31 +0530

    added new project | fire and safety

---
+++ projects/CURRENT/fire-and-safety/js/app.js
@@ -0,0 +1,16 @@
+$(function () {
+    // HOME SLIDER
+    $('#home-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        nav: false,
+        // navText: ['<i class="feather-chevron-left"></i>', '<i class="feather-chevron-right"></i>'],
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 700,
+        autoplayTimeout: 3000,
+    });
+
+
+});
