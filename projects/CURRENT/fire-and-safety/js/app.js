606db1bf2737834453038efc48191b91ac3b9782
alifrontend499 <alifrontend96@gmail.com>
2021-04-08 23:08:33 +0530

    made new changes | fire and safety | complete the home page except the testimonials

--- projects/CURRENT/fire-and-safety/js/app.js
+++ projects/CURRENT/fire-and-safety/js/app.js
@@ -12,5 +12,21 @@ $(function () {
         autoplayTimeout: 3000,
     });
 
+    // TESTIMONIAL SLIDER
+    var owl = $('#testimonial-slider').owlCarousel({
+        items: 1,
+        loop: false,
+        dots: false,
+        nav: false,
+        // navText: ['<i class="feather-chevron-left"></i>', '<i class="feather-chevron-right"></i>'],
+        autoplay: false,
+        smartSpeed: 700,
+    });
+
+    $('.owl-dot').click(function () {
+        $(this).addClass('active').siblings().removeClass('active')
+        owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+    });
+
 
 });
