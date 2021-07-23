c439a82e3355fb4e9dc8785b178363749a0f4416
alifrontend499 <alifrontend96@gmail.com>
2021-07-23 20:03:38 +0530

    changes in the new project fragrancia | new changes

--- projects/CURRENT/fragrancia/js/app.js
+++ projects/CURRENT/fragrancia/js/app.js
@@ -1,1 +1,54 @@
+$(function () {
+    // QUANTITY INCREASE DECREASE CONTROLS
+    (function () {
+        $(".quantity-select-controls > button").click(function (ev) {
+            ev.preventDefault();
+            var currentVal = parseInt($(this).parent().find(">input").val());
+            if ($(this).hasClass("plus")) {
+                if (currentVal >= 1) {
+                    $(this)
+                        .parent()
+                        .find(">input")
+                        .val(currentVal + 1);
+                } else {
+                    return false;
+                }
+            } else {
+                if (currentVal > 1) {
+                    $(this)
+                        .parent()
+                        .find(">input")
+                        .val(currentVal - 1);
+                } else {
+                    return false;
+                }
+            }
+        });
+    })();
 
+    (function () {
+        // on load
+        $(".st-form.materialize-like-input > input,.st-form.materialize-like-input > textarea").each(function () {
+            if ($(this).val().length === 0) {
+                $(this).closest(".st-form").addClass("empty");
+            } else {
+                $(this).closest(".st-form").removeClass("empty");
+            }
+        });
+
+        // on change
+        $(".st-form.materialize-like-input > input,.st-form.materialize-like-input > textarea").keyup(function () {
+            if ($(this).val().length === 0) {
+                $(this).closest(".st-form").addClass("empty");
+            } else {
+                $(this).closest(".st-form").removeClass("empty");
+            }
+        });
+
+        // on label click
+        $(".st-form.materialize-like-input > label").click(function (ev) {
+            ev.preventDefault()
+            $(this).closest('.st-form').find('>input, textarea').focus()
+        });
+    })();
+});
